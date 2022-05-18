266b91ea368f28c89f4c1203df403a649fe3a16a
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2022-05-18 10:54:25 +0300

    javascipt 1chi mashqim

---
+++ js/index.js
@@ -0,0 +1,87 @@
+//alert ("Hello wolrd");
+
+// KOMENTARIYAGA OLISH JS 2XIL BO'LADI 
+// 1)SINGLE LINE COMMENT-YANI HAR BIRIGA
+// "OTABEK"
+// 23
+
+// 2)MULTIPLE LINE COMMENT
+/*
+10 + 5
+ISM
+YOKI RAQAM
+*/
+
+
+// O'zgaruvchilar + Consulga chiqarish
+// var ism = "Otabek";
+// var yosh = 21
+// console.log("Assalomu alaykum, " + ism);
+
+
+// var ish = "Dasturchi";
+// console.log(ish);
+
+
+// var firsName = "Otabek"
+// console.log(ism, yosh);
+// camelCase; -> JS
+//JS da O'ZGARUVCHILAR CAMELCASE da YOZILADI YANI BOSH HARFI KICHIIK SO'Z BILAN KEYINGI KELUVCHI SOZ ESA KATTA VA 2LASI QO'SHIB YOZILADI 
+
+//SNAKE_CASE; ->Python
+//var first_name = "Otabek";
+
+//Pascal case;
+//var FirstName = "Otabek";
+
+//kebab-case;
+//var first name = "Mekmurod";
+
+
+
+
+// var myFirstJob = "Devoloper"
+// console.log(myFirstJob);
+// O'ZGARUVCHILARGA NOM BERISHDA ODATTA ENGLISH WORDS DAN FOYDALANISH KERAK!!!!
+
+
+
+
+   //4-lesson
+  // 1) VAZIFA 
+ const userName = prompt("What is your name?", "Jack");
+ const userSurname = prompt("What is your surname?", "Mauro");
+ const userJob = prompt("What is your job?", "Campyuter engineer");
+ let userAge = prompt("How old are you?", "I'm 21 years old");
+ let userCountry = prompt("Where are you from", "I'm from Canada");
+ let userHobby = prompt("What is your hobby?", "playing footboll");
+ let userFootballteam = prompt("What is your favorite football team?", "Barselone");
+ //Malumotlarni chiqarish
+ console.log("What is your name:? " + userName + "\n" + "What is your surname? " + userSurname + "\n" + "What is your job? " + userJob + "\n" + "How old are you? " + userAge + "\n" + "Where are you from? " + userCountry + "\n" + "What is your hobby? " + userHobby + "\n" + "What is your favorite football team? " + userFootballteam + "\n");
+
+// 2) VAZIFA
+ //Country and nationality 
+ let country = "Canada";
+ let countryPopulation = "38,370,000";
+ let countryLanguage = "English";
+ let footballPlayer = "Devis";
+ let sport = "Hackey";
+ let topUnversity = "Toronto Unversity";
+ let countryTerritory = "890,000,000";
+ console.log("Country: " + country + "\n" + "CountryPopulation: " + countryPopulation + "\n" + "CountryLanguage: " + countryLanguage + "\n" + "FootballPlayer: " + footballPlayer + "\n" + "Sport: " + sport + "\n" + "TopUnversity: " + topUnversity + "\n" + "CountryTerritory: " + countryTerritory + "\n");
+
+ // 3-VAZIFA
+ //MALUMOT TURININ ANIQLASH
+ const yosh = "yigirma yetti";
+ let unversity = "Toronto Unversity";
+ let raqam = 21
+ let aniqlash;
+ console.log(typeof yosh);
+ console.log(typeof unversity);
+ console.log(typeof raqam);
+ console.log(typeof aniqlash);
+
+
+ 
+
+
